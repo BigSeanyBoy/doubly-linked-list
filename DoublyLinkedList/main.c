ae4de6b34fa192423f3be7fa60745840be09b03f
Sean Hammell <sean.m.hammell@gmail.com>
2022-05-28 11:52:07 -0500

    Add Node struct

--- DoublyLinkedList/main.c
+++ DoublyLinkedList/main.c
@@ -6,9 +6,41 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+struct Node {
+    struct Node *previous;
+    char *message;
+    struct Node *next;
+} typedef node;
+
+int main() {
+    node *head, *tail, *temp;
+    
+    head = malloc(sizeof(struct Node));
+    head->previous = NULL;
+    head->message = NULL;
+    
+    tail = malloc(sizeof(struct Node));
+    head->message = NULL;
+    head->next = NULL;
+    
+    temp = malloc(sizeof(struct Node));
+    
+    free(head);
+    free(tail);
+    free(temp);
+    
+//    temp->previous = NULL;
+//    char *str = "Hello, List!";
+//    temp->message = (char *)malloc((strlen(str) + 1) * sizeof(char));
+//    memcpy(temp->message, str, (strlen(str) + 1) * sizeof(char));
+//    temp->next = NULL;
+//
+//    printNode(temp);
+//
+//    free(temp->message);
+    
     return 0;
 }
